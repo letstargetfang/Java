7f9ad218c227fdec3fbaba11ea98fb74876b0a9a
letstargetfang <ashwaniharjai5@gmail.com>
2024-02-09 01:52:23 +0530

    Day 4 of learning java

---
+++ day4/Assignment.java
@@ -0,0 +1,13 @@
+package day4;
+
+public class Assignment {
+    public static void main(String[] args) {
+
+        // Assignment operator "="
+        // Assignment operator
+        int  myInt = 10;
+        System.out.println(myInt);
+        int newInt = myInt;
+        System.out.println(newInt);
+    }
+}
---
+++ day4/Swap.java
@@ -0,0 +1,16 @@
+package day4;
+
+public class Swap {
+    public static void main(String[] args) {
+
+        int A = 10;
+        int B = 20;
+        System.out.println(A +" " + B);
+        // swapping
+        int Temp = A;
+        A = B;
+        B = Temp;
+        System.out.println(A +" " + B );
+
+    }
+}
